7e0ac8d9c304c995a6fdb5ba7ab217696414d5c3
kailas11 <kailaschandak11@gmail.com>
2021-04-26 22:49:21 +0530

    Updated Web application ready to deploy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
 import os
 import yaml
 import joblib
@@ -7,23 +7,23 @@ import numpy as np
 params_path = "params.yaml"
 webapp_root = "webapp"
 
-static_dir = os.path.join(webapp_root,"static")
-template_dir = os.path.join(webapp_root,"templates")
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
     return config
 
-def predict (data):
-    config = read_params(config_path)
+def predict(data):
+    config = read_params(params_path)
     model_dir_path = config["webapp_model_dir"]
     model = joblib.load(model_dir_path)
     prediction = model.predict(data)
     print(prediction)
-    return prediction
+    return prediction[0]
 
 def api_response(request):
     try:
@@ -33,21 +33,18 @@ def api_response(request):
         return response
     except Exception as e:
         print(e)
-        error ={"error": "Something went Wrong"}
+        error = {"error": "Something went wrong!! Try again"}
         return error
 
-
-
-
-@app.route("/",methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method =="POST":
+    if request.method == "POST":
         try:
             if request.form:
                 data = dict(request.form).values()
-                data = [list(map(float,data))]
+                data = [list(map(float, data))]
                 response = predict(data)
-                return render_template("index.html",response = response)
+                return render_template("index.html", response=response)
 
             elif request.json:
                 response = api_response(request)
@@ -55,11 +52,11 @@ def index():
 
         except Exception as e:
             print(e)
-            error ={"error": "Something went Wrong"}
-            return error
-            #return render_template("404.html", error=error)
+            error = {"error": "Something went wrong!! Try again"}
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True) 
