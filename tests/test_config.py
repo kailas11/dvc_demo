894d837a3cf0cf29df7b6c2bad66a5bc4cc0710c
kailas11 <kailaschandak11@gmail.com>
2021-04-28 12:03:32 +0530

    Updated workflow,requirment.txt,template

--- tests/test_config.py
+++ tests/test_config.py
@@ -69,7 +69,7 @@ def test_api_response_correct_range(data=input_data["correct_range"]):
 
 def test_form_response_incorrect_range(data=input_data["incorrect_range"]):
     with pytest.raises(prediction_service.prediction.NotInRange):
-        res = form_response(data)
+        _ = form_response(data)
 
 
 def test_api_response_incorrect_range(data=input_data["incorrect_range"]):
@@ -78,7 +78,7 @@ def test_api_response_incorrect_range(data=input_data["incorrect_range"]):
 
 def test_form_response_incorrect_col(data=input_data["incorrect_col"]):
     with pytest.raises(prediction_service.prediction.NontInColumn):
-        res = form_response(data)
+        _ = form_response(data)
 
 def test_api_response_incorrect_col(data=input_data["incorrect_col"]):
     res = api_response(data)
